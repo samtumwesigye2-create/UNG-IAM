a08632b85be4eaa25362b52aefec72b8ef67095b
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-08 01:50:04 -0400

    Provision Remote Sensor Hub identity during JANUS startup

--- entrypoint.py
+++ entrypoint.py
@@ -5,11 +5,13 @@ from main import app as console_app
 from sso_router import router as sso_router
 from janus_router import router as janus_router
 from ecosystem_permissions import seed_ecosystem_permissions
+from scripts.provision_remote_sensor_hub import main as provision_remote_sensor_hub
 
 COOKIE_NAME = "ung_iam_session"
 COOKIE_MAX_AGE = 28800
 
 seed_ecosystem_permissions()
+provision_remote_sensor_hub()
 
 
 class BrowserSessionCookieMiddleware(BaseHTTPMiddleware):
